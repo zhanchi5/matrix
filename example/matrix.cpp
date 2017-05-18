6770e01733d7febc69a6a81be639fe501df95b16
zhanchi5 <zhanchi5@outlook.com>
2017-05-19 02:12:46 +0300

    Create matrix.cpp

---
+++ example/matrix.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <iomanip>
+
+using namespace std:
+
+int main()
+{
+    Matrix<int> A(2, 4);
+    Matrix<int> B(2, 4);
+    Matrix<int> D(4, 4);
+
+    A.LoadFromFile("/home/theflash/Programming/CLionProjects/bmstu_lab7/Matrix1.txt");
+    B.LoadFromFile("/home/theflash/Programming/CLionProjects/bmstu_lab7/Matrix2.txt");
+    D.LoadFromFile("/home/theflash/Programming/CLionProjects/bmstu_lab7/Matrix3.txt");
+
+    cout << A << endl;
+    cout << B<< endl;
+    cout << D << endl;
+
+    if (A == A) cout << true << endl;
+    cout << endl;
+
+    Matrix<int> C(2,4);
+    C = A + B;
+    cout << C << endl;
+
+    C = A * D;
+    cout << C << endl;
+
+
+
+    return 0;
+}
